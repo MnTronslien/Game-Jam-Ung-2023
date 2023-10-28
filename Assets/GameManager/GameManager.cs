8d854e2d1c8deed1d3b46653ba571dd2167ad638
Tommy-ASD <tommy.aleksander.sd@gmail.com>
2023-10-28 20:33:38 +0200

    made logic to detect game over

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -42,9 +42,16 @@ public class GameManager : MonoBehaviour
     public void PlayerHealthReached0(PlayerScript player) {
         //When a player dies, we need to add score to all the other players
 
+        int alivePlayerCount = 0;
+        // if there's only 1 player left, this tracks them
+        // will only be accessed if alivePlayerCount is 1
+        PlayerInfo last = null;
+
         foreach (var p in players) {
             if (p.IsAlive) {
                 p.score += 1;
+                alivePlayerCount += 1;
+                last = p;
             }
         }
 
@@ -54,7 +61,19 @@ public class GameManager : MonoBehaviour
         //Then if there are only onle player alive, we declare a winner.
             //Pause game
             //SHow UI for winning player - this should be another scene with podium and stuff
+        if (alivePlayerCount == 1) {
+            // there's only one player left, e.g. that player has wom
+            // the 'last' variable is a reference to that player
+            if (last != null) {
+                last.score += 1;
+            };
+            FinishRound();
+        }
+    }
 
+    private void FinishRound()
+    {
+        throw new NotImplementedException();
     }
 
     private void UpdateUI()
