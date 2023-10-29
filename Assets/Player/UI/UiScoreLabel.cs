c341d87e547faeec00fc9476f0e281f8814234af
Mattias T <mntronslien@gmail.com>
2023-10-29 13:36:34 +0100

    Art pass

--- Assets/Player/UI/UiScoreLabel.cs
+++ Assets/Player/UI/UiScoreLabel.cs
@@ -1,10 +1,19 @@
 using System;
+using Microsoft.Unity.VisualStudio.Editor;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 public class UiScoreLabel : MonoBehaviour {
 
     public TMPro.TextMeshProUGUI textMesh;
+    //Unity ui image component
+    public Image deathIcon;
+
+    void Start() {
+        deathIcon.gameObject.SetActive(false);
+    }
 
     public void SetScore(int amount) {
         textMesh.text = amount.ToString();
@@ -13,5 +22,10 @@ public class UiScoreLabel : MonoBehaviour {
     internal void SetColor(Color color)
     {
         textMesh.color = color;
+    }
+
+    public void SetDead(){
+        textMesh.color = Color.Lerp(textMesh.color, Color.white, 0.5f);
+        deathIcon.gameObject.SetActive(true);
     }
 }
